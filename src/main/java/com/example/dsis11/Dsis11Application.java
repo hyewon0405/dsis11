e65ccf6c0cea4d586154da797b13c60a1bf181ad
심혜원 <shw050405@naver.com>
2025-05-25 21:54:43 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/example/dsis11/Dsis11Application.java
@@ -0,0 +1,13 @@
+package com.example.dsis11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsis11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dsis11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dsis11/Dsis11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsis11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsis11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
