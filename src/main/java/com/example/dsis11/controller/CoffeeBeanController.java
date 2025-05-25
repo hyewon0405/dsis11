106ad499c494d3312481a02c64f96759f2468105
심혜원 <shw050405@naver.com>
2025-05-26 01:07:26 +0900

    수정중

--- src/main/java/com/example/dsis11/controller/CoffeeBeanController.java
+++ src/main/java/com/example/dsis11/controller/CoffeeBeanController.java
@@ -1,13 +1,17 @@
 package com.example.dsis11.controller;
 
+import com.example.dsis11.domain.CoffeeBean;
 import com.example.dsis11.service.CoffeeBeanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/cafe")
+@RequestMapping("/coffee")
 public class CoffeeBeanController {
     private final CoffeeBeanService coffeeBeanService;
 
@@ -20,5 +24,10 @@ public class CoffeeBeanController {
     public String createCafeBean(){
         coffeeBeanService.saveTenCafeBeansEfficient();
         return "10개의 카페 데이터가 생성되었습니다.";
+    }
+
+    @GetMapping("/list")
+    public List<CoffeeBean> getCoffeeBeanList(){
+        return coffeeBeanService.getAllCoffeeBeans();
     }
 }
