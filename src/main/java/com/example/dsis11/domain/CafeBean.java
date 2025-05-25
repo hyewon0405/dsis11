7fc0e9707a750ab4703269fa6613967f880558ae
심혜원 <shw050405@naver.com>
2025-05-25 23:02:45 +0900

    수정중

--- src/main/java/com/example/dsis11/domain/CafeBean.java
+++ src/main/java/com/example/dsis11/domain/CafeBean.java
@@ -1,4 +1,27 @@
 package com.example.dsis11.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+
 public class CafeBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int quantity;
+
+    public CafeBean(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
 }
